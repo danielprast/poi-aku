c59b533be75f50e049435f2185a1fd121fe03c66
Daniel <danielprastiwa@gmail.com>
2023-11-03 09:55:59 +0700

    🏗️ Init project

---
+++ POI Aku/POI_AkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  POI_AkuApp.swift
+//  POI Aku
+//
+//  Created by Daniel Prastiwa on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct POI_AkuApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
