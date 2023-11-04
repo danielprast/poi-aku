1f8cc440247cde2670f9f082a934e80ee643868d
danielprast <danielprastiwa@gmail.com>
2023-11-04 10:31:44 +0700

    🏗️ Embed map kit view

--- Packages/PoiAkuAppSource/Sources/PoiAkuAppSource/Main/Presentation/Scene/POI/MapView.swift
+++ Packages/PoiAkuAppSource/Sources/PoiAkuAppSource/Main/Presentation/Scene/POI/MapView.swift
@@ -4,20 +4,39 @@
 //
 
 import SwiftUI
+import MapKit
 
 
 public struct MapView: View {
 
+  @EnvironmentObject var poiViewModel: PoiViewModel
+
   public init() {}
 
   public var body: some View {
-    Text("Map View")
+    ZStack {
+      MapViewHelper()
+        .environmentObject(poiViewModel)
+        .ignoresSafeArea()
+    }
   }
+
+
 }
 
 
-struct MapView_Previews: PreviewProvider {
-  static var previews: some View {
-    MapView()
+struct MapViewHelper: UIViewRepresentable {
+  @EnvironmentObject var poiViewModel: PoiViewModel
+  func makeUIView(context: Context) -> MKMapView {
+    return poiViewModel.mapView
   }
+  func updateUIView(_ uiView: MKMapView, context: Context) {}
 }
+
+
+
+//struct MapView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    MapView()
+//  }
+//}
